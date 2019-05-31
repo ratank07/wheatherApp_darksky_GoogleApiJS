b3a4d2874b1d356f9a0eecd2f1506b83f71a0da6
Ratan Karande <ratankarande700@gmail.com>
2019-05-31 08:22:33 +0530

    added env file for apikey with server code  change

--- server.js
+++ server.js
@@ -2,16 +2,18 @@ if(process.env.NODE_ENV != 'production'){
     require('dotenv').config()
 }
 
-//const DARK_API_KEY = process.env.DARKSKY_API_KEY;
-const DARK_API_KEY = d101a930f0d7bdba0fb57cf1a40b313f
-const axios = require(axios);
+
+const DARK_API_KEY = process.env.DARKSKY_API_KEY;
+//const DARK_API_KEY = d101a930f0d7bdba0fb57cf1a40b313f
+const axios = require('axios');
 const express = require('express');
 const app = express();
 app.use(express.json());
 app.use(express.static('public'));
 
 app.post('/weather', (req, res)=>{
-    const url = ` https://api.darksky.net/forecast/${DARK_API_KEY}/${req.body.latitude},${req.body.longitude}?units=auto`
+    console.log('entered in wheather');
+    const url = `https://api.darksky.net/forecast/${DARK_API_KEY}/${req.body.latitude},${req.body.longitude}?units=auto`
 console.log(req.body);
 axios({
     url: url,
